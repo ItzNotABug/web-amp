90d11ebd0fdcf7bad5187dd46bd9a355a50a34d2
ItzNotABug <itznotabug@gmail.com>
2024-07-18 14:43:24 +0530

    minor cleanups, add configs.

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import pkg from "./package.json";
-import typescript from "@rollup/plugin-typescript";
+import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
     external: Object.keys(pkg.dependencies ?? {}),
-    input: "src/index.ts",
+    input: 'src/index.ts',
     plugins: [typescript()],
     output: [
         {
-            format: "cjs",
+            format: 'cjs',
             file: pkg.main,
             esModule: false,
-            sourcemap: true,
+            sourcemap: true
         },
         {
-            format: "es",
+            format: 'es',
             file: pkg.module,
-            sourcemap: true,
+            sourcemap: true
         },
         {
-            format: "iife",
+            format: 'iife',
             file: pkg.jsdelivr,
-            name: "Appwrite",
-            extend: true,
-        },
-    ],
+            name: '@itznotabug/web-amp',
+            extend: true
+        }
+    ]
 };
